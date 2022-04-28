3507934fb2a93e58e386dfe941792025aeec1d22
Aditya Tripathi <adityatripathi.at04@gmail.com>
2022-04-29 00:39:24 +0530

    remove silly mistake

--- routes/volunteer.js
+++ routes/volunteer.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { addVolunteer, removeVolunteer, getVolunteers } = require("../controllers/volunteer");
+const {
+  addVolunteer,
+  removeVolunteer,
+  getVolunteers,
+} = require("../controllers/volunteer");
 
 const router = express.Router();
 
 router.post("/add-volunteer", addVolunteer);
-router.post("/r/v", removeVolunteer);
+router.post("/r/v/:id", removeVolunteer);
 router.get("/get-volunteers", getVolunteers);
 
 module.exports = router;
